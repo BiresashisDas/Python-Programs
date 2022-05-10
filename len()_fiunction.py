62dcb0a0fd2438347246d540255d42840c291eb8
Biresashis_das <105218699+Biresashis@users.noreply.github.com>
2022-05-10 12:07:05 +0530

    Add files via upload

---
+++ len()_fiunction.py
@@ -0,0 +1,7 @@
+#here i will show how to use a len() function.
+
+#for string
+name = input("What is your name : ")
+string = len(name)
+print("The number of characters in your name is : ",string)
+
