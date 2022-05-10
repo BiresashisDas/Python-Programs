1616ead1ddd9c1e137357988800b2dd82b9b877e
Biresashis Das <105218699+BiresashisDas@users.noreply.github.com>
2022-05-11 00:16:07 +0530

    Update Life_in_days_weeks_months.py

--- Programs/Life_in_days_weeks_months.py
+++ Programs/Life_in_days_weeks_months.py
@@ -11,3 +11,16 @@ weeks_remaining = years_remaining * 52
 months_remaining = years_remaining * 12
 
 print(f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left.")
+
+
+# Sample Input
+
+#     What is your current age? 22
+
+
+# Sample Output
+
+#     You have 24820 days, 3536 weeks, and 816 months left.
+
+
+
