1e0848606997f3057107d5d785c9da7a13ef6878
Biresashis_das <105218699+Biresashis@users.noreply.github.com>
2022-05-10 12:43:53 +0530

    Add files via upload

---
+++ len()_fiunction.py
@@ -0,0 +1,8 @@
+height = float(input("Enter your height in m: "))
+weight = float(input("Enter your weight in kg: "))
+
+#The BMI is calculated by dividing a person's weight(in kg) by the square of their height(in m)
+BMI = weight / (height**2)
+print(round(BMI))
+
+
