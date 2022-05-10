a60a33d23b62a490922380d93940386d364d8ee2
Biresashis_das <105218699+Biresashis@users.noreply.github.com>
2022-05-10 12:37:07 +0530

    Update and rename len_function.py to function.py

--- len_function.py
+++
@@ -1,8 +0,0 @@
-#here i will show how to use a len() function.
-#Note :- I will also use the type function to chech what is the class of a variable
-
-name = input("What is your name : ")
-print(type(name)) #type() function
-string = len(name)
-print("The number of characters in your name is : ",string)
-
