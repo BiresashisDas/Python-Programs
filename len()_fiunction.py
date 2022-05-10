b5413859a27ebd058e42ff0a29db19c6b670f8f3
Biresashis_das <105218699+Biresashis@users.noreply.github.com>
2022-05-10 13:20:28 +0530

    Add files via upload

---
+++ len()_fiunction.py
@@ -0,0 +1,11 @@
+#a program using maths and f-Strings that tells us how many days, weeks, months we have left if we live until 100 years old.
+age = int(input("What is your current age? "))
+
+years_remaining = 100 - age
+days_remaining = years_remaining * 365
+weeks_remaining = years_remaining * 52
+months_remaining = years_remaining * 12
+
+print(f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left.")
+
+
