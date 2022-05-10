7db41cd7938bb1f4e76e7dc01d493bd0df900bf6
Biresashis_das <105218699+Biresashis@users.noreply.github.com>
2022-05-10 12:45:25 +0530

    Update and rename len()_fiunction.py to Body_Mass_Index(BMI).py

--- Body_Mass_Index(BMI).py
+++ Body_Mass_Index(BMI).py
@@ -1,7 +1,9 @@
+#How to calculate a Body mass index.
+
 height = float(input("Enter your height in m: "))
 weight = float(input("Enter your weight in kg: "))
 
-#The BMI is calculated by dividing a person's weight(in kg) by the square of their height(in m)
+#The BMI is calculated by dividing a person's weight(in kg) by the square of their height(in m).
 BMI = weight / (height**2)
 print(round(BMI))
 
