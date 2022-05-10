c7c17e01c275264ef363d8b299aef0cf99166c12
Biresashis Das <105218699+BiresashisDas@users.noreply.github.com>
2022-05-10 21:29:00 +0530

    Update function.py

--- function.py
+++ function.py
@@ -1,4 +1,6 @@
-#here i will show how to use a Built-in function and Datatypes.
+#Author :- Biresashis Das
+
+#Here i will show how to use a Built-in function and Datatypes.
 #Note :- I will also use the type function to chech what is the class of a variable
 
 name = input("What is your name : ")
