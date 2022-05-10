f92e77ff100391438cffb77b43052509a593555f
Biresashis Das <105218699+BiresashisDas@users.noreply.github.com>
2022-05-10 21:52:08 +0530

    Update Life_in_days_weeks_months.py

--- Programs/Life_in_days_weeks_months.py
+++ Programs/Life_in_days_weeks_months.py
@@ -1,1 +1,13 @@
+# Author :- Biresashis Das
 
+#Write a program to check for how many days, weeks and months a person have left if he will live until 100 years old. 
+#We will create a program using maths and f-Strings that tells us how many days, weeks, months we have left if we live until 100 years old.
+
+age = int(input("What is your current age? "))
+
+years_remaining = 100 - age
+days_remaining = years_remaining * 365
+weeks_remaining = years_remaining * 52
+months_remaining = years_remaining * 12
+
+print(f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left.")
