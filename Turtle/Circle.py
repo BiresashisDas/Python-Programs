d6b4b75a4f2f0ff5bc767e8c2c1057b20b1ca96f
Biresashis Das <105218699+BiresashisDas@users.noreply.github.com>
2022-05-11 13:36:28 +0530

    Update Circle.py

--- Turtle/Circle.py
+++ Turtle/Circle.py
@@ -1,1 +1,19 @@
+# Author :- Biresashis Das
+
+# How to draw a circle using turtle.
+
+from turtle import Turtle, Screen
+
+bir = Turtle()
+screen = Screen()
+
+# bir.shapesize(2)  # If u need the shape than comment out this line.
+
+bir.color("red")
+bir.width(12)
+print(bir.circle(100))
+
+screen.exitonclick()
+
+
 
