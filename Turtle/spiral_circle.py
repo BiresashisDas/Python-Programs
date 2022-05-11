db218977a61d76aceeb873e9c8636c4bd1b61877
Biresashis Das <105218699+BiresashisDas@users.noreply.github.com>
2022-05-11 17:07:38 +0530

    Update spiral_circle.py

--- Turtle/spiral_circle.py
+++ Turtle/spiral_circle.py
@@ -1,1 +1,21 @@
+# Author :- Biresashis Das
+
+# How to draw a Spiral Circle
+
+from turtle import Turtle, Screen
+
+screen = Screen()
+bir = Turtle()
+bir.color("darkorange")
+bir.width(4)
+bir.hideturtle()
+
+radius = 10
+
+for i in range(125):
+    bir.circle(radius + i, 30)
+
+screen.exitonclick()
+
+
 
