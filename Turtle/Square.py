d5eea7211bc5f649367774a1117e5f01c43f3091
Biresashis Das <105218699+BiresashisDas@users.noreply.github.com>
2022-05-11 12:11:58 +0530

    Update Square.py

--- Turtle/Square.py
+++ Turtle/Square.py
@@ -1,1 +1,18 @@
+# Author :- Biresashis Das
+
+# How to create a perfect square using turtle.
+
+from turtle import Turtle, Screen
+
+bir = Turtle()
+bir.color("red")
+
+for i in range(1,5):
+    bir.forward(100)
+    bir.right(90)
+
+
+screen = Screen()
+screen.exitonclick()
+
 
