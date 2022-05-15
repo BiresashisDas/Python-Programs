9fbfecd4d76018f40c7551d9ac9057d4325dfa4e
Biresashis Das <105218699+BiresashisDas@users.noreply.github.com>
2022-05-15 18:27:20 +0530

    Update main.py

--- Simple_Game/snakegame/main.py
+++ Simple_Game/snakegame/main.py
@@ -3,6 +3,12 @@
 # This is our main file. We will run this file to start our snake game. 
 # As you guys can see how I have imported other python files in this file.
 
+'''     We will use upper arrow for up direction
+        Down arrow for down direction
+        Left arrow for direction
+        and Right arrow for right direction     '''
+
+
 from turtle import Screen
 import time
 from snake import Snake
