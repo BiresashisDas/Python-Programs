4c32ebea24a1f29822f90dbbf0dc730c7444cbea
Biresashis Das <105218699+BiresashisDas@users.noreply.github.com>
2022-05-18 16:53:08 +0530

    Update math_module.py

--- Modules/math_module.py
+++ Modules/math_module.py
@@ -1,5 +1,9 @@
-import math
+# Author :- Biresashis Das
 
+# Here I will show how to find a factorial of a number using math module.
+# Note :- You should use math module for solving mathematical equation. 
+
+import math
 
 num = int(input("Enter the number : "))
 result = math.factorial(num)
