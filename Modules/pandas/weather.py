3c85b5bc5cd706def6cc519ed3c0d772343401dc
Biresashis Das <105218699+BiresashisDas@users.noreply.github.com>
2022-05-22 12:53:07 +0530

    Update weather.py

--- Modules/pandas/weather.py
+++ Modules/pandas/weather.py
@@ -3,7 +3,9 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-print(data["Temperature	"])
+print(data["Day"])          # It will print the Day column from the weather.csv file
+print(data["Temperature"])  # It will print the Temperature column from the weather.csv file
+print(data["Weather"])      # It will print the Weather column from the weather.csv file
 
 
 
