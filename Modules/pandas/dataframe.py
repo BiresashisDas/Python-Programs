4b6ccf6898670c49ed08f88ebd01ebf29ef5eab8
Biresashis Das <105218699+BiresashisDas@users.noreply.github.com>
2022-05-25 12:59:23 +0530

    Update dataframe.py

--- Modules/pandas/dataframe.py
+++ Modules/pandas/dataframe.py
@@ -2,12 +2,12 @@
 
 #Create a Dataframe from Scratch
 
-data_dict_new = {
+data = {
     "Students": ["Ronak", "Abhishek", "Udit"],
     "Scores": [54, 76, 83]
 }
-data_new = pandas.DataFrame(data_dict_new)
-print(data_new)
+new_data = pandas.DataFrame(data)
+print(new_data)
 
 
 
