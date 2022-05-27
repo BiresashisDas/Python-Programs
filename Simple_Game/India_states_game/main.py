74d6c949fdb3867f69b5a5d02ab8a6f364353c75
Biresashis Das <105218699+BiresashisDas@users.noreply.github.com>
2022-05-27 15:07:48 +0530

    Update main.py

--- Simple_Game/India_states_game/main.py
+++ Simple_Game/India_states_game/main.py
@@ -1,1 +1,39 @@
+# Author :- Biresashis Das
+
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("INDIA States Game")
+image = "states_of_INDIA.gif"
+screen.bgpic(image)
+turtle.shape(image)
+
+data = pandas.read_csv("states_and_UT.csv")
+all_states = data.state.to_list()
+guessed_places = []
+
+while len(guessed_places) < 37:
+    states = screen.textinput(title=f"{len(guessed_places)}/37 States and UT Correct",
+                              prompt="Write the name of the state or UT?").title()
+
+    if states == "Exit":
+        break
+
+    if states in all_states:
+        guessed_places.append(states)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == states]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(states)
+
+screen.exitonclick()
+
+
+
+
+
+
 
