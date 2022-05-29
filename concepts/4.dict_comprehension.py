076440ed0e7df35e4be91f2f92e07fe2c12ad2d1
Biresashis Das <105218699+BiresashisDas@users.noreply.github.com>
2022-05-29 21:35:20 +0530

    Update 4.dict_comprehension.py

--- concepts/4.dict_comprehension.py
+++ concepts/4.dict_comprehension.py
@@ -1,1 +1,16 @@
+# Author :- Biresashis Das
+
+# Note :- We use this "{}" to make a dictionary.
+
+# Dictionary Comprehension
+
+'''
+            Basic Format :        
+                new_dict = {new_key:new_value for item in list}
+'''
+
+sentence = "There is some good in this world, and it’s worth fighting for."
+result = {line:len(line) for line in sentence.split()}
+print(result)
+
 
