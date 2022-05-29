d810faa923d7037256b48463d8877892a0a40b5d
Biresashis Das <105218699+BiresashisDas@users.noreply.github.com>
2022-05-29 21:40:26 +0530

    Update 5.conditional_dict.py

--- concepts/5.conditional_dict.py
+++ concepts/5.conditional_dict.py
@@ -8,10 +8,16 @@
 
 import random
 names = ["Amit","Vijay","Rajesh","Erik","Jason","Jeremy","Matthew"]
-student_scores = {student:random.randint(1,100) for student in names}
+student_scores = {student:random.randint(1,100) for student in names}   # Dictionary Comprehension
 print(student_scores)
 
 passed_students = {student:score for (student,score) in student_scores.items() if score >= 60}
 print(passed_students)
+
+
+# Expected Output
+
+# {'Amit': 52, 'Vijay': 66, 'Rajesh': 70, 'Erik': 64, 'Jason': 23, 'Jeremy': 98, 'Matthew': 27}
+# {'Vijay': 66, 'Rajesh': 70, 'Erik': 64, 'Jeremy': 98}
 
 
