dd7cf396c83ff37741a2c469b0f64f3527e9cfb7
Biresashis Das <105218699+BiresashisDas@users.noreply.github.com>
2022-05-29 20:38:26 +0530

    Update conditional_list.py

--- concepts/conditional_list.py
+++ concepts/conditional_list.py
@@ -1,1 +1,21 @@
+# Author :- Biresashis Das
+
+# Conditional List Comprehension
+'''
+        Basic Format :
+            list = []
+            new_list = [new_item for item in list if test]
+'''
+          
+#for integers
+numbers = [1,2,3]
+new_num = [num for num in numbers if num < 3]
+print(new_num)
+
+#for strings
+name = ["abhi", "ronak", "Ankita", "bIr", "ash"]
+new_name = [names.upper() for names in name if len(names) < 5]
+print(new_name)
+
+
 
