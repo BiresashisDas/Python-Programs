c74a7cec46f07dbb47dd45feca3ef6b4766ae971
Biresashis Das <105218699+BiresashisDas@users.noreply.github.com>
2022-05-31 12:23:32 +0530

    Update list.py

--- list/list.py
+++ list/list.py
@@ -1,1 +1,17 @@
+# Author :- Biresashis Das
+
+# Note :- We use this "[]" to make a list.
+
+number = [1,2,3]
+numbers = []
+
+for n in number:
+    new_num = n+1
+    numbers.append(new_num)
+print(numbers)
+
+
+# Expected Output
+# [2, 3, 4]
+
 
