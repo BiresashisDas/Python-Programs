8d7d9b22f0903ab39e34947c1a5d5dc299499604
Biresashis Das <105218699+BiresashisDas@users.noreply.github.com>
2022-05-31 13:27:05 +0530

    Update Table.py

--- Turtle/Table.py
+++ Turtle/Table.py
@@ -1,23 +1,21 @@
 # Author :- Biresashis Das
 
-
 # Here we will see how to create a Table using prettytable module
 
 from prettytable import PrettyTable
 table = PrettyTable()
-table.add_column("CAR BRAND", ["TATA", "TESLA", "JAGUAR",])
-table.add_column("OWNER NAME", ["SIR RATAN TATA", "ELON MUSK", "SIR RATAN TATA",])
+table.add_column("CAR BRAND", ["TATA", "MAHINDRA XUV700", "JAGUAR",])
+table.add_column("OWNER NAME", ["SIR RATAN TATA", "Mr. ANAND MAHINDRA", "SIR RATAN TATA",])
 table.align = "l"
 print(table)
 
 
 # It will show the given output
 
-#       +-----------+----------------+
-#       | CAR BRAND | OWNER NAME     |
-#       +-----------+----------------+
-#       | TATA      | SIR RATAN TATA |
-#       | TESLA     | ELON MUSK      |
-#       | JAGUAR    | SIR RATAN TATA |
-#       +-----------+----------------+
-
+#       +-----------------+--------------------+
+#       | CAR BRAND       | OWNER NAME         |
+#       +-----------------+--------------------+
+#       | TATA            | SIR RATAN TATA     |
+#       | MAHINDRA XUV700 | Mr. ANAND MAHINDRA |
+#       | JAGUAR          | SIR RATAN TATA     |
+#       +-----------------+--------------------+
