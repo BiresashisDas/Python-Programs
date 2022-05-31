89e6faa5f45341f5033c663d6934469d713a9c79
Biresashis Das <105218699+BiresashisDas@users.noreply.github.com>
2022-05-31 13:37:45 +0530

    Update 3.dataframe.py

--- Dictionary/3.dataframe.py
+++ Dictionary/3.dataframe.py
@@ -1,1 +1,37 @@
+# Author :- Biresashis Das
+
+# Here I will show you how to use loops with pandas DataFrame and iterate over a pandas DataFrame.
+
+import pandas
+
+student = {
+    "Name" : ["JACK", "MATHEW", "VIKAS"],
+    "Score" : [45, 54, 76]
+}
+
+student_dataframe = pandas.DataFrame(student)
+print(student_dataframe,"\n")
+
+
+# Loop throughs row of a DataFrame
+for (index, row) in student_dataframe.iterrows():
+    print(row)
+    
+#     Expected Output
+
+#          Name  Score
+#     0    JACK     45
+#     1  MATHEW     54
+#     2   VIKAS     76 
+
+#     Name     JACK
+#     Score      45
+#     Name: 0, dtype: object
+#     Name     MATHEW
+#     Score        54
+#     Name: 1, dtype: object
+#     Name     VIKAS
+#     Score       76
+#     Name: 2, dtype: object
+
 
