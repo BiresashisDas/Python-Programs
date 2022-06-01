d1d9f73a489394018802493dffcac9bc0eb806cb
Biresashis Das <105218699+BiresashisDas@users.noreply.github.com>
2022-06-01 13:12:57 +0530

    Update main.py

--- Programs/nato_phonetic_alphabet/main.py
+++ Programs/nato_phonetic_alphabet/main.py
@@ -1,1 +1,13 @@
+# Author :- Biresashis Das
+
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+new_data = {row.letter:row.code for (index, row) in data.iterrows()}
+
+word = input("Enter a word : ").upper()
+result = [new_data[letter] for letter in word]
+print(result)
+
+
 
