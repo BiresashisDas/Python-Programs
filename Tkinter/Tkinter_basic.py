05c69adb7c1fbf6190edcc8836a831293e297851
Biresashis Das <105218699+BiresashisDas@users.noreply.github.com>
2022-06-08 12:50:19 +0530

    Update Tkinter_basic.py

--- Tkinter/Tkinter_basic.py
+++ Tkinter/Tkinter_basic.py
@@ -1,1 +1,15 @@
+# Author :- Biresashis Das
+
+import tkinter
+
+windows = tkinter.Tk()
+windows.title("My Fisrt GUI")
+windows.minsize(width=600, height=600)
+
+lable = tkinter.Label(text= "This is a Normal Text", font=("Arial",15,"bold"))
+lable.pack(side="top")
+
+windows.mainloop()
+
+
 
