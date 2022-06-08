57388740fcc16fb4a01e38f5e74dae07140d5aa0
Biresashis Das <105218699+BiresashisDas@users.noreply.github.com>
2022-06-08 12:55:46 +0530

    Update kwargs.py

--- Tkinter/kwargs.py
+++ Tkinter/kwargs.py
@@ -1,1 +1,11 @@
+# Author :- Biresashis Das
+
+def calculate(n, **kwargs):
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+# Here n is 4, so 4 is first added to 3 and then the result of their addition will be multiplied by 6 and it will give us the output 42.
+calculate(4, add=3, multiply=6)
+
 
