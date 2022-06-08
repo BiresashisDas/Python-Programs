d7f69333c30e256790b9e1b6e9ccb1be36c2f9a0
Biresashis Das <105218699+BiresashisDas@users.noreply.github.com>
2022-06-08 12:58:21 +0530

    Update kwargs_class.py

--- Tkinter/kwargs_class.py
+++ Tkinter/kwargs_class.py
@@ -1,1 +1,15 @@
+# Author :- Biresashis Das
+
+class Sports():
+    def __init__(self, **kwargs):
+        self.sports = kwargs.get("sports_name")
+        self.name = kwargs.get("cand_name")
+        self.country = kwargs.get("country_name")
+
+my_sports = Sports(sports_name="Javelin", cand_name="Neeraj Chopra")
+print(my_sports.sports)
+print(my_sports.country)    #This will print 'None'.
+
+
+
 
